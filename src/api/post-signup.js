2deac9b4aff7bdc4beec1d5fff4817c7a0c79218
GinaHorch <reggiaus@hotmail.com>
2024-12-06 15:13:38 +0800

    Address 404 error - add backend url to api

--- src/api/post-signup.js
+++ src/api/post-signup.js
@@ -1,7 +1,7 @@
 import axios from "axios"; // Use axios for making HTTP requests
 
 // Define the base URL for the API
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://culture4kids-7a814d1e1904.herokuapp.com/";
 
 async function postSignup(signupData) {
   try {
