49a3c3af9a6d5e0e522a70ce7ed8124aa96d2eab
GinaHorch <reggiaus@hotmail.com>
2024-12-06 17:36:19 +0800

    Temporarily hard coded Heroku URL into post-signup to debug fronted 404 issue

--- src/api/post-signup.js
+++ src/api/post-signup.js
@@ -1,7 +1,7 @@
 import axios from "axios"; // Use axios for making HTTP requests
 
 // Define the base URL for the API
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://culture4kids-7a814d1e1904.herokuapp.com/";
+const API_BASE_URL = "https://culture4kids-7a814d1e1904.herokuapp.com/"; // add after debugging if everything works process.env.REACT_APP_API_BASE_URL || 
 
 async function postSignup(signupData) {
   try {
