2b0a7d013388895b5f892ffea1cc2996ca1b9c93
GinaHorch <reggiaus@hotmail.com>
2024-12-06 08:31:16 +0800

    Add SignupPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />;
             </Routes>
         </Router>
     );
